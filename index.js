deef842fd0da9fce64ae40283df1853520131fcf
Marco <marcorf164@gmail.com>
2023-07-03 20:57:31 -0600

    se arrgelan los comentarios

--- index.js
+++ index.js
@@ -1,42 +1,48 @@
-let expresion = "";
-// se declara la variable expresion con un valor indefinido 
-function limpiar() {
-    // se crea la función limpiar
-    expresion = "";
-    // se le asigna un valor indefinido a la variable expresion
-    actualizarPantalla();
-    // se ejecuta la función actualizar pantalla
+
+let expresion = ""; // se declara la variable expresion con un valor indefinido 
+
+/**
+ * @description Función que limpia pantalla
+ */
+function limpiar() { 
+    expresion = ""; // se le asigna un valor indefinido a la variable expresion
+    actualizarPantalla(); // se ejecuta la función actualizar pantalla
 }
 
+/**
+ *@params Recibe un parámetro de tipo número
+ *@description función concatenarResultado 
+ * */
 function concatenarResultado(numero) {
-    // se crea la función concatenarResultado con el parámetro numero
-    expresion += numero;
-    // se utiliza += para ir concatenando el número que pulsemos
-    actualizarPantalla();
-    // se ejecuta la función actualizar pantalla
+    expresion += numero; // se utiliza += para ir concatenando el número que pulsemos
+    actualizarPantalla(); // se ejecuta la función actualizar pantalla
 }
+
+/**
+ *@params Recibe un string de nombre operador
+ *@description función concatenarOperador 
+ * */
 function concatenarOperador(operador) {
-    // se crea la función concatenarOperador con el parámetro operador
-    expresion += operador;
-    // se utiliza += para ir concatenando el operador que pulsemos
-    actualizarPantalla();
-    // se ejecuta la función actualizar pantalla
+    expresion += operador;  // se utiliza += para ir concatenando el operador que pulsemos
+    actualizarPantalla(); // se ejecuta la función actualizar pantalla
 }
-function actualizarPantalla() {
-    // se crea la función actualizarPantalla
-    document.getElementById("display").value = expresion;
-    // se le dice que vaya tomando los valores de los botones que están en el html y se los pase a la variable expresion 
+
+/**
+ * @description función actualizarPantalla
+ */
+function actualizarPantalla() { 
+    document.getElementById("display").value = expresion; // se le dice que vaya tomando los valores de los botones que están en el html y se los pase a la variable expresion 
 }
-function calcular() {
-    // se crea la función calcular
+
+/**
+ * @description función calcular
+ */
+function calcular() { 
     try {
-        const resultado = eval(expresion);
-        //se crea la constante resultado y se le asigna el valor
-        expresion = resultado.toString();
-        actualizarPantalla();
-    } catch (error) {
-        //si ocurre un error entra aquí
-        console.log(error)
-        // se imprime el mensaje error en la consola
+        const resultado = eval(expresion); //se crea la constante resultado
+        expresion = resultado.toString(); //convierte el resultado en un string 
+        actualizarPantalla(); //se imprime el string en la pantalla
+    } catch (error) { //si ocurre un error entra aquí
+        console.log(error) // se imprime el mensaje error en la consola    
     }
 }
